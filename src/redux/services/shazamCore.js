7cc3ff5c0ac4f24360f41f00f411a0be0e6a52a4
zekero1 <rjayocon06@gmail.com>
2023-04-27 15:49:20 +0800

    discover page and music player configuration with design

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,21 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const shazamCoreApi = createApi({
+	reducerPath: 'shazamCoreApi',
+	baseQuery: fetchBaseQuery({
+		baseUrl: 'https://shazam.p.rapidapi.com/',
+		prepareHeaders: (headers) => {
+			headers.set(
+				'X-RapidAPI-Key',
+				'4fdde24a54msh249deb3565fe0ecp13bac4jsn1c4448a30513'
+			);
+
+			return headers;
+		},
+	}),
+	endpoints: (builder) => ({
+		getTopCharts: builder.query({ query: () => '/charts/track' }),
+	}),
+});
+
+export const { useGetTopChartsQuery } = shazamCoreApi;
