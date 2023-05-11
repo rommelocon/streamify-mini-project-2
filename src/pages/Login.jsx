8635a515a95eb12067171aa6bab06d09df87e821
zekero1 <rjayocon06@gmail.com>
2023-05-11 20:47:41 +0800

    fix login and register with minor bugs and functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,48 +1,42 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import Validation from './Validation';
 import ApiService from '../components/ApiService';
 
 function Login() {
-	const [values, setValues] = useState({
+	const [credentials, setCredentials] = useState({
 		email: '',
 		password: '',
 	});
 
-	const [errors, setErrors] = useState({});
-
-	const [accounts, setAccounts] = useState([]);
-
-	const getAccountList = () => {
-		ApiService('/accounts', null, (data) => {
-			setAccounts(data);
-		});
-	};
-
-	useEffect(() => {
-		getAccountList();
-	}, []);
+	const [user, setUser] = useState(null);
 
 	const handleInput = (event) => {
-		setValues((prev) => ({
+		setCredentials((prev) => ({
 			...prev,
-			[event.target.name]: [event.target.value],
+			[event.target.name]: event.target.value,
 		}));
 	};
 
 	const handleSubmit = (event) => {
 		event.preventDefault();
-		setErrors(Validation(values));
+		ApiService(
+			'/accounts/login',
+			credentials,
+			(data) => {
+				setCredentials({ email: '', password: '' });
+				setUser(data.user);
+			},
+			'POST'
+		);
 	};
 
-	console.log(accounts);
-
+	console.log(user);
 	return (
-		<div className='flex justify-center align-middle h-auto bg-transparent'>
-			<div className='text-white p-3 rounded w-25'>
+		<div className='flex justify-center items-center'>
+			<div className='text-white p-10 rounded-xl w-full bg-black bg-opacity-20 backdrop-blur-lg'>
 				<h2 className='text-2xl text-center font-bold p-3'>Login</h2>
-				<form action='' onSubmit={handleSubmit}>
-					<div className='grid grid-cols-2 gap-4 mb-3 justify-center items-center'>
+				<form onSubmit={handleSubmit}>
+					<div className='grid grid-cols-4 gap-4 mb-3 justify-center items-center'>
 						<label
 							htmlFor='email'
 							className='text-base font-semibold tracking-wide'
@@ -51,16 +45,14 @@ function Login() {
 						</label>
 						<input
 							type='email'
-							className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4'
+							className='col-span-3 flex-1 bg-transparent border border-gray-600 rounded placeholder-gray-500 text-base text-white p-4 '
+							value={credentials.email}
 							onChange={handleInput}
 							placeholder='Enter Email'
 							name='email'
 						/>
-						{errors.email && (
-							<span className='text-danger'> {errors.email}</span>
-						)}
 					</div>
-					<div className='grid grid-cols-2 gap-4 mb-3 justify-center items-center'>
+					<div className='grid grid-cols-4 gap-4 mb-3 justify-center items-center'>
 						<label
 							htmlFor='password'
 							className='text-base font-semibold tracking-wide'
@@ -69,25 +61,29 @@ function Login() {
 						</label>
 						<input
 							type='password'
-							className='flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4'
+							className='col-span-3 flex-1 bg-transparent border border-gray-600 rounded placeholder-gray-500 text-base text-white p-4'
+							value={credentials.password}
 							onChange={handleInput}
 							placeholder='Enter Password'
 							name='password'
 						/>
-						{errors.password && (
-							<span className='text-danger'>{errors.password}</span>
+
+						{user && (
+							<div>
+								Welcome, {user.firstName} {user.lastName}
+							</div>
 						)}
 					</div>
 					<div class='grid grid-cols-2 gap-4 mb-3 justify-center items-center text-center'>
 						<button
 							type='submit'
-							className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded'
+							className='bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded'
 						>
 							Login
 						</button>
 						<Link
 							to='/register'
-							className='bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded'
+							className='bg-white hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded'
 						>
 							Register
 						</Link>
