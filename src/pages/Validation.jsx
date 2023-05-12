7ba13962a621621f344517afbca4a60d5b0fe2f2
arcosesd2 <bkmoonsalter@gmail.com>
2023-05-12 19:32:19 +0800

    Added Confirm Password does not match.

--- src/pages/Validation.jsx
+++ src/pages/Validation.jsx
@@ -1,28 +1,21 @@
-function Validation(values){
-    let error = {}
-    const email_pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9]{8,}$/
+function Validation(values) {
+	let error = {};
 
-    if(values.email === ""){
-        error.email = "Email should not be empty"
-    }
-    else if(!email_pattern.test(values.email)){
-        error.email = "Email didn't match"
-    }else {
-        error.email = ""
-    }
+	const password_pattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
 
-    if(values.password === "") {
-        error.password = "Password should not be empty"
-    }
-    else if(!password_pattern.test(values.password)) {
-        error.password = "Password didn't match"
-    } else{
-        error.password = ""
-    }
+	if (values.password === '') {
+		error.password = 'Password should not be empty';
+	} else if (!password_pattern.test(values.password)) {
+		error.password = "Password must contain at least 1 number, 1 lowercase, 1 uppercase, 1 special character and at least 8 characters";
+	} else {
+		error.password = '';
+	}
 
-    return error;
+	if (values.password != values.confirmpassword) {
+		error.confirmpassword = "Password does not match!"
+	}
 
+	return error;
 }
 
 export default Validation;
